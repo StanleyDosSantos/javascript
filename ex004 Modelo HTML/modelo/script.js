3c0ac3c266297c4cc194b50e8525e9794fd6326d
Stanley dos Santos Cornélio <stanley15731@gmail.com>
2024-04-17 20:39:16 -0300

    atualizei a pasta

--- ex004 Modelo HTML/modelo/script.js
+++ ex004 Modelo HTML/modelo/script.js
@@ -5,15 +5,15 @@ function carregar() {
 
     var data = new Date();
 
-    //var hora = data.getHours();
-    var hora = 13
+    var hora = data.getHours();
+    //var hora = 20
     msg.innerHTML = `Agora são ${hora} horas.`;
     if (hora >= 0 && hora < 12) {
-        img.src = 'imagens/manha3.jpg';
+        img.src = 'manha3.jpg';
     } else if (hora >= 12 && hora <= 18) {
-        img.src = 'imagens/tarde3.jgp';
+        img.src = 'tarde3.jpg';
     } else {
-        img.src = 'imagens/noite3.jgp';
+        img.src = 'noite3.jpg'
     }
 
 }
