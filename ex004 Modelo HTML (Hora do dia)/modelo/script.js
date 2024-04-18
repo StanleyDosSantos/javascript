e045f02f7aa6b0badf51df562c21e4a0f9195a9f
Stanley dos Santos Cornélio <stanley15731@gmail.com>
2024-04-18 12:50:53 -0300

    terminei o ex modelo

--- ex004 Modelo HTML (Hora do dia)/modelo/script.js
+++ ex004 Modelo HTML (Hora do dia)/modelo/script.js
@@ -6,14 +6,17 @@ function carregar() {
     var data = new Date();
 
     var hora = data.getHours();
-    //var hora = 20
+    //var hora = 21
     msg.innerHTML = `Agora são ${hora} horas.`;
     if (hora >= 0 && hora < 12) {
         img.src = 'manha3.jpg';
+        document.body.style.background = '#e2cd9f';
     } else if (hora >= 12 && hora <= 18) {
         img.src = 'tarde3.jpg';
+        document.body.style.background = '#b9846f';
     } else {
         img.src = 'noite3.jpg'
+        document.body.style.background = '#515154';
     }
 
 }
