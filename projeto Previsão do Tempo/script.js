e15d2373a12e3aa27ed0278d02a440b8ba5d86e1
Stanley dos Santos Cornélio <stanley15731@gmail.com>
2024-04-27 19:46:17 -0300

    projeto criado

--- projeto Previsão do Tempo/script.js
+++ projeto Previsão do Tempo/script.js
@@ -5,7 +5,10 @@ const key = "bd29dabececfa7a2431f1e9f1e8301bb"
 function colocarDadosNaTela(dados) {
     console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em  " + dados.name
-    document.querySelector(".temp").innerHTML = dados.main.temp
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "ºC"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 
 async function buscarCidade(cidade) {
