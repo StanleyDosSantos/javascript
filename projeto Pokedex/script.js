b73ed39460ca9bd19b001f5b453b51e13a9072b1
Stanley dos Santos Cornélio <stanley15731@gmail.com>
2024-08-26 19:28:30 -0300

    continuando o projeto

--- projeto Pokedex/script.js
+++ projeto Pokedex/script.js
@@ -2,19 +2,27 @@ const pokemonName = document.querySelector('.pokemon__name');
 const pokemonNumber = document.querySelector('.pokemon__number');
 const pokemonImage = document.querySelector('.pokemon__image');
 
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+
 const fetchPokemon = async (pokemon) => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await APIResponse.json();
     return data;
 }
 
-const renderPOkemon = async (pokemon) => {
+const renderPokemon = async (pokemon) => {
 
     const data = await fetchPokemon(pokemon);
 
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 
-} 
+}
 
-renderPOkemon('13')
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemon(input.value);
+});
