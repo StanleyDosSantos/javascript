3c82d49e209d1139ba5034c2e209a13235f4d1e6
Stanley dos Santos Cornélio <stanley15731@gmail.com>
2024-10-17 19:52:31 -0300

    dando continuidade

---
+++ projeto Troca de cores/script.js
@@ -0,0 +1,12 @@
+
+
+function  trocaCor(cor){
+    document.body.style.backgroundColor = cor
+}
+
+function corAleatoria(){
+    const red = Math.floor(Math.random() * 255)
+    Console.log(red)
+
+    document.body.style.backgroundColor
+}
